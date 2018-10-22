fa85cdef1e05bc6e1c1e03062e8df8820c2f020c
Jagdeesh Basavaraju <jagdeeshb14@gmail.com>
2018-10-21 19:06:14 -0700

    Assignment 3 completed

--- Assignment_3/Assignment3_Interface.py
+++ Assignment_3/Assignment3_Interface.py
@@ -10,18 +10,14 @@ import threading
 
 # Donot close the connection inside this file i.e. do not perform openconnection.close()
 def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
-    #Implement ParallelSort Here.
+    #Function to Parallel sort the InputTable on SortingColumnName and store results in OutputTable
 	con = openconnection
 	cur = con.cursor()
 	query_str = "SELECT MAX(" + SortingColumnName + "), MIN(" + SortingColumnName + ") FROM " + InputTable + ";"
-	print("Query: " + query_str)
 	cur.execute(query_str)
 	max_col_val, min_col_val = cur.fetchone()
-	print("Max: " + str(max_col_val))
-	print("Min: " + str(min_col_val))
 	num_threads = 5
-	delta = (max_col_val - min_col_val) / num_threads
-	print("Delta: " + str(delta))
+	delta = float(max_col_val - min_col_val) / num_threads
 	TEMP_TABLE_PREFIX = "temp_table_sort"
 	thread_list = [0] * num_threads
 	for i in range(num_threads):
@@ -29,54 +25,47 @@ def ParallelSort (InputTable, SortingColumnName, OutputTable, openconnection):
 		max_range = min_range + delta
 		thread_list[i] = threading.Thread(target=sortTable, args=(InputTable, TEMP_TABLE_PREFIX, SortingColumnName, min_range, max_range, i, openconnection))
 		thread_list[i].start()
-		temp_table_name = TEMP_TABLE_PREFIX + str(i)
 	cur.execute("DROP TABLE IF EXISTS " + OutputTable + ";")
-	query_str = "CREATE TABLE " + OutputTable + " ( LIKE " + InputTable + " INCLUDING ALL);"
-	print(query_str)
+	query_str = "CREATE TABLE " + OutputTable + " ( LIKE " + InputTable + " INCLUDING ALL );"
 	cur.execute(query_str)
 	for i in range(num_threads):
 		thread_list[i].join()
 		temp_table_name = TEMP_TABLE_PREFIX + str(i)
-		query_str = "INSERT INTO " + OutputTable +" SELECT * FROM " + temp_table_name + ";"	
-		print(query_str)
+		query_str = "INSERT INTO " + OutputTable +" SELECT * FROM " + temp_table_name + ";"
 		cur.execute(query_str)
+	#Below 3 lines commented are just for verification	
+	#cur.execute("copy " + OutputTable + " to '/home/user/sort_output.txt';")
+	#cur.execute("SELECT * from " + "(SELECT * FROM " + InputTable + ") as st where st.movieid not in (select movieid from "+ OutputTable +");")
+	#print(cur.fetchall())
 
 def sortTable(InputTable, temp_table_prefix, SortingColumnName, min_range, max_range, i, openconnection):
+	#Function to sort a single partition based on SortingColumnName using a thread
 	con = openconnection	
 	cur = con.cursor()
 	temp_table_name = temp_table_prefix + str(i)
 	cur.execute("DROP TABLE IF EXISTS " + temp_table_name + ";")
 	query_str = "CREATE TABLE " + temp_table_name + " ( LIKE " + InputTable + " INCLUDING ALL);"
-	print(query_str)
 	cur.execute(query_str)
 	if i==0:
 		query_str = "INSERT INTO " + temp_table_name +" SELECT * FROM " + InputTable + " WHERE " + SortingColumnName + " >= "+ str(min_range) + " AND " + SortingColumnName + " <= " + str(max_range) + " ORDER BY " + SortingColumnName + " ASC;"
 	else:
 		query_str = "INSERT INTO " + temp_table_name +" SELECT * FROM " + InputTable + " WHERE " + SortingColumnName + " > "+ str(min_range) + " AND " + SortingColumnName + " <= " + str(max_range) + " ORDER BY " + SortingColumnName + " ASC;"
-	print(query_str)
 	cur.execute(query_str)
 
 def ParallelJoin (InputTable1, InputTable2, Table1JoinColumn, Table2JoinColumn, OutputTable, openconnection):
-    #Implement ParallelJoin Here.
+    #Function to Parallel join InputTable1 and InputTable2 on Table1JoinColumn and Table2JoinColumn and store results in OutputTable
 	con = openconnection
 	cur = con.cursor()
 	query_str = "SELECT MAX(" + Table1JoinColumn + "), MIN(" + Table1JoinColumn + ") FROM " + InputTable1 + ";"
-	print("Query: " + query_str)
 	cur.execute(query_str)
 	max_col_val1, min_col_val1 = cur.fetchone()
-	print("Max1: " + str(max_col_val1))
-	print("Min1: " + str(min_col_val1))
 	query_str = "SELECT MAX(" + Table2JoinColumn + "), MIN(" + Table2JoinColumn + ") FROM " + InputTable2 + ";"
-	print("Query: " + query_str)
 	cur.execute(query_str)
 	max_col_val2, min_col_val2 = cur.fetchone()
-	print("Max2: " + str(max_col_val2))
-	print("Min2: " + str(min_col_val2))
 	num_threads = 5
 	max_col_val = max(max_col_val1, max_col_val2)
 	min_col_val = min(min_col_val1, min_col_val2)
-	delta = (max_col_val - min_col_val) / num_threads
-	print("Delta: " + str(delta))
+	delta = float(max_col_val - min_col_val) / num_threads
 	query_str = "SELECT COLUMN_NAME, DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = '" + InputTable1 + "';"
 	cur.execute(query_str)
 	schema1 = cur.fetchall()
@@ -94,7 +83,6 @@ def ParallelJoin (InputTable1, InputTable2, Table1JoinColumn, Table2JoinColumn, 
 		thread_list[i].start()
 	cur.execute("DROP TABLE IF EXISTS " + OutputTable + ";")
 	query_str = "CREATE TABLE " + OutputTable + " ( LIKE " + InputTable1 + " INCLUDING ALL);"
-	print(query_str)
 	cur.execute(query_str)
 	query_str = "ALTER TABLE " + OutputTable + " "
 	for j in range(len(schema2)):
@@ -102,7 +90,6 @@ def ParallelJoin (InputTable1, InputTable2, Table1JoinColumn, Table2JoinColumn, 
 			query_str += "ADD COLUMN " + schema2[j][0] + " " + schema2[j][1] + ","
 		else:
 			query_str += "ADD COLUMN " + schema2[j][0] + " " + schema2[j][1] + ";"
-	print(query_str)
 	cur.execute(query_str)
 	for i in range(num_threads):
 		thread_list[i].join()
@@ -110,10 +97,14 @@ def ParallelJoin (InputTable1, InputTable2, Table1JoinColumn, Table2JoinColumn, 
 		temp_table2_name = TEMP_TABLE2_PREFIX + str(i)
 		temp_output_table_name = TEMP_OUTPUT_TABLE_PREFIX + str(i)
 		query_str = "INSERT INTO " + OutputTable +" SELECT * FROM " + temp_output_table_name + ";"	
-		print(query_str)
 		cur.execute(query_str)
+	#Below 3 lines commented are just for verification	
+	#cur.execute("copy " + OutputTable + " to '/home/user/join_output.txt';")
+	#cur.execute("SELECT * from " + "(SELECT * FROM " + InputTable1 + " INNER JOIN " + InputTable2 + " ON " + InputTable1 + "." + Table1JoinColumn + " = " + InputTable2 + "." + Table2JoinColumn + ") as jt where jt.movieid not in (select movieid from "+ OutputTable +");")
+	#print(cur.fetchall())
 	
 def joinTable(InputTable1, InputTable2, schema1, schema2, temp_table1_prefix, temp_table2_prefix, Table1JoinColumn, Table2JoinColumn, temp_output_table_prefix, min_range, max_range, i, openconnection):
+	#Function to join two single partition based on Table1JoinColumn and Table2JoinColumn using a thread	
 	con = openconnection	
 	cur = con.cursor()
 	temp_table1_name = temp_table1_prefix + str(i)
@@ -121,15 +112,12 @@ def joinTable(InputTable1, InputTable2, schema1, schema2, temp_table1_prefix, te
 	temp_output_table_name = temp_output_table_prefix + str(i)
 	cur.execute("DROP TABLE IF EXISTS " + temp_table1_name + ";")
 	query_str = "CREATE TABLE " + temp_table1_name + " ( LIKE " + InputTable1 + " INCLUDING ALL);"
-	print(query_str)
 	cur.execute(query_str)
 	cur.execute("DROP TABLE IF EXISTS " + temp_table2_name + ";")
 	query_str = "CREATE TABLE " + temp_table2_name + " ( LIKE " + InputTable2 + " INCLUDING ALL);"
-	print(query_str)
 	cur.execute(query_str)
 	cur.execute("DROP TABLE IF EXISTS " + temp_output_table_name + ";")
 	query_str = "CREATE TABLE " + temp_output_table_name + " ( LIKE " + InputTable1 + " INCLUDING ALL);"
-	print(query_str)
 	cur.execute(query_str)
 	query_str = "ALTER TABLE " + temp_output_table_name + " "
 	for j in range(len(schema2)):
@@ -137,24 +125,18 @@ def joinTable(InputTable1, InputTable2, schema1, schema2, temp_table1_prefix, te
 			query_str += "ADD COLUMN " + schema2[j][0] + " " + schema2[j][1] + ","
 		else:
 			query_str += "ADD COLUMN " + schema2[j][0] + " " + schema2[j][1] + ";"
-	print(query_str)
 	cur.execute(query_str)
 	if i==0:
 		query_str = "INSERT INTO " + temp_table1_name +" SELECT * FROM " + InputTable1 + " WHERE " + Table1JoinColumn + " >= "+ str(min_range) + " AND " + Table1JoinColumn + " <= " + str(max_range) + ";"
-		print(query_str)
 		cur.execute(query_str)
 		query_str = "INSERT INTO " + temp_table2_name +" SELECT * FROM " + InputTable2 + " WHERE " + Table2JoinColumn + " >= "+ str(min_range) + " AND " + Table2JoinColumn + " <= " + str(max_range) + ";"
-		print(query_str)
 		cur.execute(query_str)
 	else:
 		query_str = "INSERT INTO " + temp_table1_name +" SELECT * FROM " + InputTable1 + " WHERE " + Table1JoinColumn + " > "+ str(min_range) + " AND " + Table1JoinColumn + " <= " + str(max_range) + ";"
-		print(query_str)
 		cur.execute(query_str)
 		query_str = "INSERT INTO " + temp_table2_name +" SELECT * FROM " + InputTable2 + " WHERE " + Table2JoinColumn + " > "+ str(min_range) + " AND " + Table2JoinColumn + " <= " + str(max_range) + ";"
-		print(query_str)
 		cur.execute(query_str)
 	query_str = "INSERT INTO " + temp_output_table_name + " SELECT * FROM " + temp_table1_name + " INNER JOIN " + temp_table2_name + " ON " + temp_table1_name + "." + Table1JoinColumn + " = " + temp_table2_name + "." + Table2JoinColumn + ";"
-	print(query_str)
 	cur.execute(query_str)
 
 ################### DO NOT CHANGE ANYTHING BELOW THIS #############################
